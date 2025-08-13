0d850471fb1dd71f922e12b87529e1973381e9c9
iza266 <isabelly.santos26@escola.pr.gov.br>
2025-08-12 22:14:53 -0300

    ,,

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Conhecimento Gerais',
+    'Quem foi o quadrinista responsável pela criação da Liga da Justiça?',
+    'O Gardner Fox'
+)
+
+criaCartao(
+    'Conhcimento Gerais',
+    'Quais desses heróis não estavam na primeira formação da Liga da Justiça?',
+    'Arqueiro Verde'
+)
+
+criaCartao(
+    'Conhecimento Gerais',
+    'Qual das bases secretas da Liga da Justiça também foi usada pela Justiça Jovem?',
+    'O Santuário Secreto'
+)
